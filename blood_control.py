4d55c86e24feda5fee2df062b46ab3d7aa437bd2
ShubhamPatwar <sp.patwar16@gmail.com>
2025-12-17 15:22:27 +0530

    imrpvoed blood 2

--- blood_control.py
+++ blood_control.py
@@ -48,7 +48,7 @@ def run_motor_sequence_blood():
 
         if common.stop_flag: return
         pause_event.wait()
-        timer_module.start_timer(1200)
+        timer_module.start_timer(4*60)
         # mixer(2,1200)        #1200####### LYSIS ########
         wash_mixer(2,4*60)
         if common.stop_flag: return
